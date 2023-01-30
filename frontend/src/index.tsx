70c26996fd0b137c749a184bd58732703a188562
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2023-01-31 02:05:37 +0900

    Pages init (#1)
    * init browser router
    
    * init react router dom
    
    * clean
    
    * clean
    
    * update
    
    * update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { Matches } from "./Matches";
+import { Login } from "./Login";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <div><Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/matches" element={<Matches />} />
+          </Routes></div>
+      </BrowserRouter>
   </React.StrictMode>
 );
