436fc3d96359291c1eda75b814b2d407129fb47d
katataku <katataku.dev@gmail.com>
2023-02-01 01:06:41 +0900

    fix/remove console.log

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -33,13 +33,13 @@ export class EventsGateway {
   ): string {
     const room: string = data;
     this.logger.log(`channelNotification`);
-    console.log('room notify:' + room);
-    console.log('clientList.get(room):' + this.clientList.get(room));
+    this.logger.log('room notify:' + room);
+    this.logger.log('clientList.get(room):' + this.clientList.get(room));
     if (this.clientList.get(room) === undefined) {
       this.clientList[room] = [];
     }
     this.clientList[room].push(client);
-    console.log('clientList.get(room):' + this.clientList.get(room));
+    this.logger.log('clientList.get(room):' + this.clientList.get(room));
     return data;
   }
 
