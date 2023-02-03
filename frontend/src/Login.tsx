d3f3c77656c49f9c0b4751ee69c7c58133314a1e
katataku <katataku.dev@gmail.com>
2023-02-04 01:07:37 +0900

    fix:frontend lint

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,6 +1,6 @@
 import React, { type ReactElement } from 'react'
 // import axios from 'axios'
-export function Login (): ReactElement {
+export function Login(): ReactElement {
   return (
     <div className="Login">
       <button onClick={req}>Log in</button>
@@ -8,7 +8,7 @@ export function Login (): ReactElement {
   )
 }
 
-function req (): void {
+function req(): void {
   // const res = await axios.get('http://localhost:3001/api')
   // console.log(res.data)
 }
