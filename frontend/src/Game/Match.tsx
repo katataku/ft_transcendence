c99a581e93b0017e980ba152059eace8c4eb9556
shima8823 <taiyaki1275@gmail.com>
2023-02-04 19:42:05 +0900

    [add] one paddle and reset

--- frontend/src/Game/Match.tsx
+++ frontend/src/Game/Match.tsx
@@ -1,32 +1,61 @@
-import React, { type ReactElement, useState } from 'react'
-import { useAnimationFrame } from '../utils'
-import './Match.css'
+/* eslint-disable */
+import React, { type ReactElement, useState, useEffect } from 'react';
+import { useAnimationFrame } from '../utils';
+import './Match.css';
 // import axios from 'axios'
 
 interface Vector2 {
-  x: number
-  y: number
+  x: number;
+  y: number;
 }
 
-interface ball {
-  pos: Vector2
-  vel: Vector2
+interface IBall {
+  pos: Vector2;
+  vel: Vector2;
 }
 
-interface paddle {
-  pos: Vector2
-  dir: number
+interface IPaddle {
+  pos: Vector2;
 }
 
-const wid: number = 800
-const hght: number = 500
-const ballPx: number = 20
-const initBall: ball = {
+const wid: number = 800;
+const hght: number = 500;
+const ballPx: number = 20;
+const paddleSize: Vector2 = {
+  x: 8,
+  y: 100,
+};
+const paddleSpeed: number = 10;
+const initBall: IBall = {
   pos: { x: wid / 2, y: hght / 2 },
-  vel: { x: -233, y: 235 }
+  vel: { x: -233, y: 235 },
+};
+const initPaddle: IPaddle = {
+  pos: { x: wid / 20, y: hght / 2 - paddleSize.y / 2 },
+};
+const deepCpInitBall = () => {
+  return JSON.parse(JSON.stringify(initBall)); // deep copy
+};
+
+let keydown = '';
+
+function Paddle(props: { paddle: IPaddle }) {
+  return (
+    <div
+      style={{
+        backgroundColor: 'red',
+        width: `${paddleSize.x}px`,
+        height: `${paddleSize.y}px`,
+        position: 'absolute',
+        top: `${props.paddle.pos.y}px`,
+        left: `${props.paddle.pos.x}px`,
+      }}
+      id="paddle"
+    />
+  );
 }
 
-function Ball (props: { pBall: ball }): ReactElement {
+function Ball(props: { pBall: IBall }): ReactElement {
   return (
     <div
       style={{
@@ -35,62 +64,129 @@ function Ball (props: { pBall: ball }): ReactElement {
         top: `${props.pBall.pos.y}px`,
         left: `${props.pBall.pos.x}px`,
         position: 'absolute',
-        backgroundColor: 'white'
+        backgroundColor: 'blue',
       }}
       id="ball"
     />
-  )
+  );
 }
 
-function updateBall (pBall: ball, deltaTime: number, speed: number): ball {
-  pBall.pos.x += pBall.vel.x * deltaTime * speed
-  pBall.pos.y += pBall.vel.y * deltaTime * speed
+function updateBall(
+  pBall: IBall,
+  deltaTime: number,
+  speed: number,
+  paddle: IPaddle,
+  handleScoreChange: Function,
+): IBall {
+  pBall.pos.x += pBall.vel.x * deltaTime * speed;
+  pBall.pos.y += pBall.vel.y * deltaTime * speed;
   if (pBall.pos.y <= 0 && pBall.vel.y < 0) {
-    pBall.vel.y *= -1
+    pBall.vel.y *= -1;
+  } else if (pBall.pos.y >= hght - ballPx && pBall.vel.y > 0) {
+    pBall.vel.y *= -1;
+  } else if (
+    // goal hit
+    (pBall.pos.x <= 0 && pBall.vel.x < 0) ||
+    (pBall.pos.x >= wid - ballPx && pBall.vel.x > 0)
+  ) {
+    pBall.pos = deepCpInitBall().pos;
+    pBall.vel.x *= -1;
+    handleScoreChange();
+  } else if (
+    // paddle hit
+    paddle.pos.x <= pBall.pos.x + ballPx &&
+    pBall.pos.x <= paddle.pos.x + paddleSize.x &&
+    paddle.pos.y <= pBall.pos.y + ballPx &&
+    pBall.pos.y <= paddle.pos.y + paddleSize.y
+  ) {
+    pBall.vel.x *= -1;
   }
-  if (pBall.pos.y >= hght - ballPx && pBall.vel.y > 0) {
-    pBall.vel.y *= -1
+  return pBall;
+}
+function updatePaddle(paddle: IPaddle) {
+  switch (keydown) {
+    case 'ArrowUp':
+      if (paddle.pos.y >= paddleSpeed) paddle.pos.y += -paddleSpeed;
+      break;
+    case 'ArrowDown':
+      if (paddle.pos.y <= hght - paddleSize.y - paddleSpeed)
+        paddle.pos.y += paddleSpeed;
+      break;
+    default:
+      break;
   }
-  if (pBall.pos.x <= 0 && pBall.vel.x < 0) {
-    pBall.vel.x *= -1
-  }
-  if (pBall.pos.x >= wid - ballPx && pBall.vel.x > 0) {
-    pBall.vel.x *= -1
-  }
-  return pBall
+  return paddle;
 }
 
-function Game (): ReactElement {
-  const [ticks, setTicks] = useState<number>(0)
-  const [pBall, setPBall] = useState<ball>(initBall)
-  const [speed, setSpeed] = useState<number>(1)
+function Game(props: { handleScoreChange: Function }): ReactElement {
+  const [ticks, setTicks] = useState<number>(0);
+  const [pBall, setPBall] = useState<IBall>(deepCpInitBall());
+  const [speed, setSpeed] = useState<number>(1);
+  const [paddle, setPaddle] = useState<IPaddle>(initPaddle);
 
-  // そのcallbackはupdateGame()のような関数です
+  //   そのcallbackはupdateGame()のような関数です
   useAnimationFrame((time: number, deltaTime: number) => {
-    const newBall = updateBall(pBall, deltaTime, speed)
-    setPBall(newBall)
-    setTicks(time)
-  })
+    const newBall = updateBall(
+      pBall,
+      deltaTime,
+      speed,
+      paddle,
+      props.handleScoreChange,
+    );
+    const newPaddle = updatePaddle(paddle);
+    setPBall(newBall);
+    setPaddle(newPaddle);
+    setTicks(time);
+    console.log(paddle.pos.y);
+  });
 
   return (
     <div id="game">
       <Ball pBall={pBall} />
+      <Paddle paddle={paddle} />
     </div>
-  )
+  );
 }
 
-export function Match (): ReactElement {
+export function Match(): ReactElement {
+  const [score, setScore] = useState<number>(0);
+  const handleScoreChange = () => {
+    setScore((score) => {
+      console.log(score + 1);
+      return score + 1;
+    });
+  };
+
+  useEffect(() => {
+    if (score === 10) {
+      console.log('GAME SET');
+    }
+  }, [score]);
+
+  const handleOnKeyDonw = (e: KeyboardEvent) => {
+    keydown = e.code;
+  };
+
+  const handleOnKeyUp = (e: KeyboardEvent) => {
+    keydown = '';
+  };
+
+  window.addEventListener('keydown', handleOnKeyDonw);
+  window.addEventListener('keyup', handleOnKeyUp);
   return (
     <div id="page">
       <div id="header">
         <button onClick={req}>click</button>
       </div>
-      <Game/>
+      <div id="score">
+        <p>score: {score}</p>
+      </div>
+      <Game handleScoreChange={handleScoreChange} />
     </div>
-  )
+  );
 }
 
-function req (): void {
+function req(): void {
   // const res = await axios.get('http://localhost:3001/api')
   // console.log(res.data)
 }
