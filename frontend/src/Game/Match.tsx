d261789a4871648b6d164475b4023a4884ba3407
shima <57759015+shima8823@users.noreply.github.com>
2023-02-06 09:15:28 +0900

    Feat/15 game paddle (#39)
    * [add] one paddle and reset
    
    * [add] right paddle
    
    * [fix] lint error
    
    * [fix] lint warn
    
    * [update] keyboard event only sets once
    
    * [fix] ball goes into the paddle

--- frontend/src/Game/Match.tsx
+++ frontend/src/Game/Match.tsx
@@ -1,4 +1,10 @@
-import React, {type ReactElement, type ChangeEvent, useState, useRef} from 'react'
+import React, {
+  type ReactElement,
+  type ChangeEvent,
+  useState,
+  useRef,
+  useEffect
+} from 'react'
 import { useAnimationFrame } from '../utils'
 import './Match.css'
 // import axios from 'axios'
@@ -8,25 +14,59 @@ interface Vector2 {
   y: number
 }
 
-interface ball {
+interface IBall {
   pos: Vector2
   vel: Vector2
 }
 
-interface _paddle {
+interface IPaddle {
   pos: Vector2
-  dir: number
 }
 
 const gameWinWid: number = 800
 const gameWinHght: number = 500
 const ballPx: number = 20
-const initBall: ball = {
-  pos: { x: (gameWinWid / 2) - (ballPx / 2), y: gameWinHght / 2 },
+const paddleSize: Vector2 = {
+  x: 8,
+  y: 100
+}
+const paddleSpeed: number = 10
+const initBall: IBall = {
+  pos: { x: gameWinWid / 2 - ballPx / 2, y: gameWinHght / 2 },
   vel: { x: -233, y: 235 }
 }
+const initLeftPaddle: IPaddle = {
+  pos: { x: gameWinWid / 20, y: gameWinHght / 2 - paddleSize.y / 2 }
+}
+const initRightPaddle: IPaddle = {
+  pos: {
+    x: gameWinWid - (gameWinWid / 20 + paddleSize.x),
+    y: gameWinHght / 2 - paddleSize.y / 2
+  }
+}
+const deepCpInitBall = (): IBall => {
+  return JSON.parse(JSON.stringify(initBall)) // deep copy of Object
+}
 
-function Ball(props: { pBall: ball }): ReactElement {
+let keydown = ''
+
+function Paddle(props: { paddle: IPaddle }): ReactElement {
+  return (
+    <div
+      style={{
+        backgroundColor: 'white',
+        width: `${paddleSize.x}px`,
+        height: `${paddleSize.y}px`,
+        position: 'absolute',
+        top: `${props.paddle.pos.y}px`,
+        left: `${props.paddle.pos.x}px`
+      }}
+      id="paddle"
+    />
+  )
+}
+
+function Ball(props: { pBall: IBall }): ReactElement {
   return (
     <div
       style={{
@@ -42,34 +82,84 @@ function Ball(props: { pBall: ball }): ReactElement {
   )
 }
 
-function updateBall(pBall: ball, deltaTime: number, speed: number): ball {
+function updateBall(
+  pBall: IBall,
+  deltaTime: number,
+  speed: number,
+  leftPaddle: IPaddle,
+  rightPaddle: IPaddle,
+  handleScoreChange: () => void
+): IBall {
   pBall.pos.x += pBall.vel.x * deltaTime * speed
   pBall.pos.y += pBall.vel.y * deltaTime * speed
   if (pBall.pos.y <= 0 && pBall.vel.y < 0) {
     pBall.vel.y *= -1
-  }
-  if (pBall.pos.y >= gameWinHght - ballPx && pBall.vel.y > 0) {
+  } else if (pBall.pos.y >= gameWinHght - ballPx && pBall.vel.y > 0) {
     pBall.vel.y *= -1
-  }
-  if (pBall.pos.x <= 0 && pBall.vel.x < 0) {
+  } else if (
+    // goal hit
+    (pBall.pos.x <= 0 && pBall.vel.x < 0) ||
+    (pBall.pos.x >= gameWinWid - ballPx && pBall.vel.x > 0)
+  ) {
+    pBall.pos = deepCpInitBall().pos
     pBall.vel.x *= -1
-  }
-  if (pBall.pos.x >= gameWinWid - ballPx && pBall.vel.x > 0) {
+    handleScoreChange()
+  } else if (
+    // paddle hit
+    (pBall.vel.x < 0 &&
+      leftPaddle.pos.x <= pBall.pos.x + ballPx &&
+      pBall.pos.x <= leftPaddle.pos.x + paddleSize.x &&
+      leftPaddle.pos.y <= pBall.pos.y + ballPx &&
+      pBall.pos.y <= leftPaddle.pos.y + paddleSize.y) ||
+    (pBall.vel.x > 0 &&
+      rightPaddle.pos.x <= pBall.pos.x + ballPx &&
+      pBall.pos.x <= rightPaddle.pos.x + paddleSize.x &&
+      rightPaddle.pos.y <= pBall.pos.y + ballPx &&
+      pBall.pos.y <= rightPaddle.pos.y + paddleSize.y)
+  ) {
     pBall.vel.x *= -1
   }
   return pBall
 }
+function updatePaddle(paddle: IPaddle): IPaddle {
+  switch (keydown) {
+    case 'ArrowUp':
+      if (paddle.pos.y >= paddleSpeed) paddle.pos.y += -paddleSpeed
+      break
+    case 'ArrowDown':
+      if (paddle.pos.y <= gameWinHght - paddleSize.y - paddleSpeed) {
+        paddle.pos.y += paddleSpeed
+      }
+      break
+    default:
+      break
+  }
+  return paddle
+}
 
-function Game(): ReactElement {
+function Game(props: { handleScoreChange: () => void }): ReactElement {
   const [_ticks, setTicks] = useState<number>(0)
-  const [pBall, setPBall] = useState<ball>(initBall)
+  const [pBall, setPBall] = useState<IBall>(deepCpInitBall())
   const p1Score = useRef<number>(0)
+  const [leftPaddle, setLeftPaddle] = useState<IPaddle>(initLeftPaddle)
+  const [rightPaddle, setRightPaddle] = useState<IPaddle>(initRightPaddle)
   const p2Score = useRef<number>(0)
   const speed = useRef<number>(1)
 
-  // そのcallbackはupdateGame()のような関数です
+  //   そのcallbackはupdateGame()のような関数です
   useAnimationFrame((time: number, deltaTime: number) => {
-    const newBall = updateBall(pBall, deltaTime, speed.current)
+    const newLeftPaddle = updatePaddle(leftPaddle)
+    const newRightPaddle = updatePaddle(rightPaddle)
+    const newBall = updateBall(
+      pBall,
+      deltaTime,
+      speed.current,
+      newLeftPaddle,
+      newRightPaddle,
+      props.handleScoreChange
+    )
+    setLeftPaddle(newLeftPaddle)
+    setRightPaddle(newRightPaddle)
     setPBall(newBall)
     setTicks(time)
   })
@@ -77,13 +167,13 @@ function Game(): ReactElement {
   const modifySpeed = (e: ChangeEvent<HTMLSelectElement>): void => {
     console.log(typeof e.target.value)
     switch (e.target.value) {
-      case("easy"):
+      case 'easy':
         speed.current = 1
         break
-      case("medium"):
+      case 'medium':
         speed.current = 2
         break
-      case("hard"):
+      case 'hard':
         speed.current = 3
         break
     }
@@ -103,17 +193,47 @@ function Game(): ReactElement {
       </div>
       <div id="gameDiv"></div>
       <Ball pBall={pBall} />
+      <Paddle paddle={leftPaddle} />
+      <Paddle paddle={rightPaddle} />
     </div>
   )
 }
 
 export function Match(): ReactElement {
+  const [score, setScore] = useState<number>(0)
+  const handleScoreChange = (): void => {
+    setScore((score) => {
+      console.log(score + 1)
+      return score + 1
+    })
+  }
+
+  useEffect(() => {
+    if (score === 10) {
+      console.log('GAME SET')
+    }
+  }, [score])
+
+  useEffect(() => {
+    const handleOnKeyDown = (e: KeyboardEvent): void => {
+      keydown = e.code
+    }
+    const handleOnKeyUp = (): void => {
+      keydown = ''
+    }
+    window.addEventListener('keydown', handleOnKeyDown)
+    window.addEventListener('keyup', handleOnKeyUp)
+  }, [])
+
   return (
     <div id="page">
       <div id="header">
         <button onClick={req}>click</button>
       </div>
-      <Game />
+      <div id="score">
+        <p>score: {score}</p>
+      </div>
+      <Game handleScoreChange={handleScoreChange} />
     </div>
   )
 }
