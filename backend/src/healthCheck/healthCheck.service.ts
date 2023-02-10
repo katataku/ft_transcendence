443d496ae104ddf9d1bf136cc3ce67ad07d4354c
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2023-02-10 19:07:16 +0900

    Revert "lint"
    This reverts commit 71702d15f60798cebb6d50a3244d6c0e1b95c3fa.

--- backend/src/healthCheck/healthCheck.service.ts
+++ backend/src/healthCheck/healthCheck.service.ts
@@ -4,28 +4,23 @@ import { Repository } from 'typeorm';
 import { HealthCheck } from '../entities/healthCheck.entity';
 import { HealthDto } from 'src/common/dto/health.dto';
 
-const healthContent = 'OK';
+const healthContent: string = 'OK'
 
 @Injectable()
 export class HealthCheckService {
   constructor(
     @InjectRepository(HealthCheck)
     private healthCheckRepository: Repository<HealthCheck>,
-  ) {
-    this.healthCheckRepository
-      .save({ health: healthContent })
-      .then(() => {
-        Logger.debug('Health Check Seeded');
+    ) {
+      this.healthCheckRepository.save({health: healthContent}).then(res => {
+        Logger.debug('Health Check Seeded')
+      }).catch(err => {
+        Logger.error('Faild to Seed')
       })
-      .catch(() => {
-        Logger.error('Faild to Seed');
-      });
   }
 
   async get(): Promise<HealthDto> {
-    const data: HealthCheck = await this.healthCheckRepository.findOne({
-      where: { health: healthContent },
-    });
+    const data: HealthCheck = await this.healthCheckRepository.findOne({where: {health: healthContent}});
     if (data == null) {
       throw new HttpException('Not found.', HttpStatus.NOT_FOUND);
     }
