c78fc9bf346642507e68423fa680a0ba896088c7
tharaguc <tharaguc@users.noreply.github.com>
2023-02-16 06:39:23 +0000

    GitHub Actions から差分を Push

--- backend/src/common/params/user.params.ts
+++ backend/src/common/params/user.params.ts
@@ -1,6 +1,6 @@
-import { IsNumberString } from "class-validator";
+import { IsNumberString } from 'class-validator';
 
 export class UserGetParam {
-	@IsNumberString()
-	id: number
+  @IsNumberString()
+  id: number;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); //CORS
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
 bootstrap();
