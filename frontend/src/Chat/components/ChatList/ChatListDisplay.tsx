a841fa6142cef54196711ab67690b6426061fef0
katataku <katataku.dev@gmail.com>
2023-02-22 18:17:59 +0900

    wip/JoinButton

--- frontend/src/Chat/components/ChatList/ChatListDisplay.tsx
+++ frontend/src/Chat/components/ChatList/ChatListDisplay.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom'
-import { type ReactElement } from 'react'
+import { useEffect, useState, type ReactElement } from 'react'
 import { Badge, Button } from 'react-bootstrap'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_HTTP_BASE_URL
 
 // フリー素材のアイコン。
 // ただし、LICENSEで再配布が禁止されているため、publicディレクトリに保存せずに、画像URLへ直接リンクする。
@@ -52,6 +55,59 @@ const EnterButton = (props: { user: User; room: ChatRoom }): JSX.Element => {
   )
 }
 
+const JoinButton = (props: { user: User; room: ChatRoom }): JSX.Element => {
+  const room = props.room
+  const user = props.user
+  const [joinButton, setJoinButton] = useState<JSX.Element>(<></>)
+
+  const handleJoinRoom = (): void => {
+    const requestData: ChatRoomMember = {
+      chatRoomId: room.id,
+      userId: user.id,
+      isBanned: false
+    }
+    axios
+      .post<ChatRoom>('/chatRoomMembers', requestData)
+      .then((_response) => {})
+      .catch((reason) => {
+        alert('エラーです！')
+        console.log(reason)
+      })
+  }
+
+  useEffect(() => {
+    const joinButtonElement: JSX.Element = (
+      <Button
+        onClick={() => {
+          handleJoinRoom()
+        }}
+      >
+        Join
+      </Button>
+    )
+
+    axios
+      .get<ChatRoomMember[]>('/chatRoomMembers')
+      .then((response) => {
+        const isMember: boolean =
+          response.data
+            .filter((item) => item.chatRoomId === room.id)
+            .filter((item) => item.userId === user.id).length === 0
+        if (isMember) {
+          setJoinButton(joinButtonElement)
+        } else {
+          setJoinButton(<></>)
+        }
+      })
+      .catch((reason) => {
+        alert('エラーです！')
+        console.log(reason)
+      })
+  }, [room, user])
+
+  return joinButton
+}
+
 // チャットルームのオーナーのみ、チャットルームの設定を変更できる。
 // そのため、チャットルームのオーナーのみ、設定ボタンを表示する。
 // また、チャットルームのオーナーのみ、チャットルームを削除できる。
@@ -81,6 +137,7 @@ const SettingButton = (props: { room: ChatRoom; user: User }): JSX.Element => {
   return icon
 }
 
+// チャットルームの一覧を表示する。
 export const ChatListDisplay = (props: {
   user: User
   roomList: ChatRoom[]
@@ -95,6 +152,7 @@ export const ChatListDisplay = (props: {
             <OwnerIcon room={room} user={props.user}></OwnerIcon>
             <IsPublicIcon room={room}></IsPublicIcon>
             <EnterButton user={props.user} room={room}></EnterButton>
+            <JoinButton user={props.user} room={room}></JoinButton>
             <SettingButton room={room} user={props.user}></SettingButton>
           </li>
         )
