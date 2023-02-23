17e418b694d8933fed851ba9301f8733a7af7bcc
Toshiki_H <85535980+tharaguc@users.noreply.github.com>
2023-02-23 19:11:50 +0900

    [User] Delete, Update (#132)
    * [add] swagger
    
    * [add] user update
    
    * [add] delete user
    
    * GitHub Actions から差分を Push
    
    ---------
    
    Co-authored-by: tharaguc <tharaguc@users.noreply.github.com>

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,12 +1,21 @@
-import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import {
   FriendRequestDto,
   UserCreateReqDto,
   UserCreateResDto,
+  UserUpdateReqDto,
   UserGetDto,
 } from 'src/common/dto/users.dto';
-import { UserGetParam } from 'src/common/params/user.params';
+import { UserIdParam } from 'src/common/params/user.params';
 
 @Controller('user')
 export class UsersController {
@@ -23,8 +32,21 @@ export class UsersController {
   }
 
   @Get(':id')
-  getUserById(@Param() param: UserGetParam): Promise<UserGetDto> {
+  getUserById(@Param() param: UserIdParam): Promise<UserGetDto> {
     return this.service.getUserById(param.id);
+  }
+
+  @Post(':id')
+  updateUser(
+    @Body() body: UserUpdateReqDto,
+    @Param() param: UserIdParam,
+  ): Promise<UserGetDto> {
+    return this.service.updateUser(param.id, body);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param() param: UserIdParam): Promise<string> {
+    return this.service.deleteUser(param.id);
   }
 
   @Post('friends')
@@ -33,12 +55,12 @@ export class UsersController {
   }
 
   @Get('friends/:id')
-  getFriendsById(@Param() param: UserGetParam): Promise<UserGetDto[]> {
+  getFriendsById(@Param() param: UserIdParam): Promise<UserGetDto[]> {
     return this.service.getFriendsById(param.id);
   }
 
   @Get('friends/pending/:id')
-  getPendingFriends(@Param() param: UserGetParam): Promise<UserGetDto[]> {
+  getPendingFriends(@Param() param: UserIdParam): Promise<UserGetDto[]> {
     Logger.log(param);
     const list = this.service.getPendingFriends(param.id);
     return list;
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -4,6 +4,7 @@ import {
   FriendRequestDto,
   UserCreateReqDto,
   UserCreateResDto,
+  UserUpdateReqDto,
   UserGetDto,
 } from 'src/common/dto/users.dto';
 import { Friendship, PendingFriendship, User } from '../entities/users.entity';
@@ -22,11 +23,10 @@ export class UsersService {
   ) {}
 
   async create(data: UserCreateReqDto): Promise<UserCreateResDto> {
-    const passHash = SHA256(data.password).toString();
     const obj: User = {
       id: null,
       name: data.name,
-      password: passHash,
+      password: SHA256(data.password).toString(),
       createdAt: new Date(),
     };
     const saved = await this.usersRepository.save(obj);
@@ -56,6 +56,25 @@ export class UsersService {
       name: data.name,
     };
     return res;
+  }
+
+  async updateUser(id: number, data: UserUpdateReqDto): Promise<UserGetDto> {
+    const target = await this.usersRepository.findOne({ where: { id: id } });
+    if (target == null) {
+      throw new HttpException('User Not Found.', HttpStatus.NOT_FOUND);
+    }
+    target.name = data.name;
+    target.password = SHA256(data.password).toString();
+    await this.usersRepository.save(target);
+    return this.getUserById(id);
+  }
+
+  async deleteUser(id: number): Promise<string> {
+    const target = await this.usersRepository.findOne({ where: { id: id } });
+    if (target == null) {
+      throw new HttpException('User Not Found.', HttpStatus.NOT_FOUND);
+    }
+    return this.usersRepository.remove(target) ? 'OK' : '...';
   }
 
   async requestFriendship(data: FriendRequestDto): Promise<void> {
