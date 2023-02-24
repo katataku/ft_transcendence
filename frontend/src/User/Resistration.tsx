5966148b5382be95a31a337d81e752b418b06ec3
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2023-02-24 18:17:57 +0900

    [add] userを作成

--- frontend/src/User/Resistration.tsx
+++ frontend/src/User/Resistration.tsx
@@ -1,7 +1,15 @@
 import React, { type ReactElement } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import axios from 'axios'
 
 type Setter<T> = React.Dispatch<React.SetStateAction<T>>
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_HTTP_BASE_URL
+
+async function createUser(data: createUser): Promise<any> {
+  const res = await axios.post('/user', data)
+  return res
+}
 
 export function Resistration(props: {
   user: User
@@ -9,18 +17,26 @@ export function Resistration(props: {
   setLoggedIn: Setter<boolean>
 }): ReactElement {
   let userName = ''
+  let password = ''
   return (
     <div>
       <Form.Control
         placeholder="UserName"
         onChange={(e) => {
           userName = e.target.value
-          // props.setUser({...props.user, name: e.target.value})
         }}
-      />
+        />
+      <Form.Control
+        placeholder="Password"
+        onChange={(e) => {
+          password = e.target.value
+        }}
+        />
       <Button
         onClick={() => {
-          props.setUser({ ...props.user, name: userName })
+          createUser({name: userName, password: password}).then(res => {
+            props.setUser({ id: Number(res.data.id), name: userName })
+          }).catch(() => {/**/})
           props.setLoggedIn(true)
         }}
       >
