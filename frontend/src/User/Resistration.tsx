c52e165ec61a53f28ab7a4c51abbbe65d3f28c3b
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2023-02-24 15:29:30 +0900

    [add] loginによる切り替え

---
+++ frontend/src/User/Resistration.tsx
@@ -0,0 +1,27 @@
+import React, { type ReactElement } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+type Setter<T> = React.Dispatch<React.SetStateAction<T>>
+
+export function Resistration(props: {user: User; setUser: Setter<User>; setLoggedIn: Setter<boolean> }): ReactElement {
+	let userName = ''
+	return (
+		<div>
+			<Form.Control
+				placeholder='UserName'
+				onChange={e => {
+					userName = e.target.value
+					// props.setUser({...props.user, name: e.target.value})
+				}}
+				/>
+			<Button
+				onClick={() => {
+					props.setUser({...props.user, name: userName})
+					props.setLoggedIn(true)
+				}}
+				>
+				Submit
+			</Button>
+		</div>
+	)
+}
