da10011cc91e93e4443822ea0fc69ac43a556cb8
Toshiki_H <85535980+tharaguc@users.noreply.github.com>
2023-02-25 10:27:38 +0900

    [User] frontendからuserを作成 (#134)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,56 +1,56 @@
-import React, { type ReactElement, useState } from 'react'
+import React, { type ReactElement, useState, useEffect } from 'react'
 // import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { Resistration } from './User/Resistration'
 
 export function App(): ReactElement {
   const ChatListState: ChatListState = { kicked: false }
+  const [loggedIn, setLoggedIn] = useState<boolean>(false)
   const [user, setUser] = useState<User>({
-    id: 1,
-    name: ''
+    id: 0,
+    name: 'hoge'
   })
+
+  useEffect(() => {
+    console.log(user)
+  }, [loggedIn])
 
   // プロフィル/チャット->ゲーム をナビゲートされる人のユーザー情報は必要があります
   // 下の<Link to="Game" state={user}>のようでできます
-  return (
-    <div className="App">
-      <button onClick={req}>req</button>
-      React
-      <Form.Control
-        placeholder="UserName"
-        onChange={(e) => {
-          setUser({ ...user, name: e.target.value })
-        }}
-      />
-      <Button
-        onClick={() => {
-          setUser({ ...user, name: user.name })
-        }}
-      >
-        Submit
-      </Button>
-      <p>
-        <Link to="chatlist" state={{ ChatListState }}>
-          Move to ChatList
-        </Link>
-      </p>
-      <p>
-        <Link to="MatchList">Move to MatchList</Link>
-      </p>
-      <p>
-        <Link to="Game" state={user}>
-          Move to Game
-        </Link>
-      </p>
-      <p>
-        <Link to="Login">Move to Login</Link>
-      </p>
-    </div>
-  )
-}
-
-function req(): void {
-  // const res = await axios.get('http://localhost:3001/api')
-  // console.log(res.data)
+  if (loggedIn) {
+    return (
+      <div className="App">
+        <p>ID : {user.id}</p>
+        <p>NAME: {user.name}</p>
+        <p>
+          <Link to="chatlist" state={{ ChatListState }}>
+            Move to ChatList
+          </Link>
+        </p>
+        <p>
+          <Link to="MatchList">Move to MatchList</Link>
+        </p>
+        <p>
+          <Link to="Game" state={user}>
+            Move to Game
+          </Link>
+        </p>
+        <p>
+          <Button
+            onClick={() => {
+              setLoggedIn(false)
+            }}
+          >
+            Log out
+          </Button>
+        </p>
+      </div>
+    )
+  } else {
+    return (
+      <Resistration user={user} setUser={setUser} setLoggedIn={setLoggedIn} />
+    )
+  }
 }
---
+++ frontend/src/User/Resistration.tsx
@@ -0,0 +1,51 @@
+import React, { type ReactElement } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import axios from 'axios'
+
+type Setter<T> = React.Dispatch<React.SetStateAction<T>>
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_HTTP_BASE_URL
+
+async function createUser(data: createUser): Promise<any> {
+  const res = await axios.post('/user', data)
+  return res
+}
+
+export function Resistration(props: {
+  user: User
+  setUser: Setter<User>
+  setLoggedIn: Setter<boolean>
+}): ReactElement {
+  let userName = ''
+  let password = ''
+  return (
+    <div>
+      <Form.Control
+        placeholder="UserName"
+        onChange={(e) => {
+          userName = e.target.value
+        }}
+      />
+      <Form.Control
+        placeholder="Password"
+        onChange={(e) => {
+          password = e.target.value
+        }}
+      />
+      <Button
+        onClick={() => {
+          createUser({ name: userName, password })
+            .then((res) => {
+              props.setUser({ id: Number(res.data.id), name: userName })
+            })
+            .catch(() => {
+              /**/
+            })
+          props.setLoggedIn(true)
+        }}
+      >
+        Submit
+      </Button>
+    </div>
+  )
+}
