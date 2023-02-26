0a62f8b764cdd89fbfb87370ffcbabd181a4c45a
shima8823 <taiyaki1275@gmail.com>
2023-02-26 16:49:21 +0900

    [fix] module name

--- backend/src/common/app.module.ts
+++ backend/src/common/app.module.ts
@@ -7,7 +7,7 @@ import { ChatModule } from '../chat';
 import { ChatMuteUserModule } from '../chatMuteUser';
 import * as dotenv from 'dotenv';
 import { ChatMuteUser } from '../entities/chatMuteUser.entity';
-import { UsersModule } from 'src/Users';
+import { UsersModule } from 'src/users';
 import { Friendship, User, PendingFriendship } from 'src/entities/users.entity';
 import { ChatRoom } from 'src/entities/chatRoom.entity';
 import { ChatRoomModule } from 'src/chatRoom';
