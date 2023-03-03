ffd21a842ed9058da2cb67baaa4d739d3bcaaa4b
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2023-03-03 14:05:54 +0900

    [add] スタイリング

--- frontend/src/User/components/SignIn.tsx
+++ frontend/src/User/components/SignIn.tsx
@@ -11,68 +11,84 @@ export function SignIn(props: {
   const [signUpMode, setSignUpMode] = useState<boolean>(true)
   const [userName, setUserName] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [showPassword, setShowPassword] = useState<boolean>(false)
   const [image, setImage] = useState<string>('')
 
-  function toggle(): void {
+  function toggleMode(): void {
     setSignUpMode(!signUpMode)
   }
 
+  function toggleShowPassword(): void {
+    setShowPassword(!showPassword)
+  }
+
   return (
-    <div>
+    <div style={{margin: '50px 100px', textAlign: 'center'}}>
       <h2>{signUpMode ? 'Sign Up' : 'Sign In'}</h2>
       <div>
         {
           signUpMode
-          ? <div>Already have an account?<Button variant='link' onClick={toggle}>Sign In</Button></div>
-          : <div>Don&apos;t have an account<Button variant='link' onClick={toggle}>Sign Up</Button></div>
+          ? <div>Already have an account?<Button variant='link' onClick={toggleMode}>Sign In</Button></div>
+          : <div>Don&apos;t have an account<Button variant='link' onClick={toggleMode}>Sign Up</Button></div>
         }
       </div>
-      <Form.Control
-        placeholder="UserName"
-        onChange={(e) => {
-          setUserName(e.target.value)
-        }}
-      />
-      <Form.Control
-        placeholder="Password"
-        type="password"
-        onChange={(e) => {
-          setPassword(e.target.value)
-        }}
-      />
-      <Form.Control
-        type="file"
-        accept="imgage/png"
-        onChange={(e) => {
-          const file = (e.target as HTMLInputElement).files?.[0] as File
-          resizeAndEncode(file)
-            .then((res) => {
-              setImage(res)
-            })
-            .catch((err) => {
-              console.error(err)
-              alert('Onl .png is accepted.')
-              setImage('')
-            })
-        }}
-      />
-      <Img src={image} style={{ borderRadius: '50%' }} height={300} />
-      <br />
-      <Button
-        onClick={() => {
-          console.log(image)
-          createUser({ name: userName, password, avatar: image })
-            .then((res) => {
-              props.setUser({ id: Number(res.data.id), name: userName })
-            })
-            .catch(() => {
-              /**/
-            })
-          props.setSignedIn(true)
-        }}
-      >
-        Submit
-      </Button>
+      <div style={{width: '500px', margin:'auto'}}>
+        <Form.Control
+          placeholder="UserName"
+          onChange={(e) => {
+            setUserName(e.target.value)
+          }}
+        />
+        <Form.Control
+          placeholder="Password"
+          type={showPassword ? 'text' : 'password'}
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+        />
+        <Form.Check
+          type='switch'
+          label='show'
+          style={{width:'100px'}}
+          onChange={toggleShowPassword}
+        />
+        {
+          signUpMode ?
+          <Form.Control
+            type="file"
+            accept="imgage/png"
+            onChange={(e) => {
+              const file = (e.target as HTMLInputElement).files?.[0] as File
+              resizeAndEncode(file)
+                .then((res) => {
+                  setImage(res)
+                })
+                .catch((err) => {
+                  console.error(err)
+                  alert('Onl .png is accepted.')
+                  setImage('')
+                })
+            }}
+          /> : <></>
+        }
+        <Img src={image} style={{ borderRadius: '50%' }} height={300} />
+        <br />
+        <Button
+          onClick={() => {
+            console.log(image)
+            createUser({ name: userName, password, avatar: image })
+              .then((res) => {
+                props.setUser({ id: Number(res.data.id), name: userName })
+              })
+              .catch(() => {
+                /**/
+              })
+            props.setSignedIn(true)
+          }}
+        >
+          Submit
+        </Button>
+      </div>
     </div>
   )
 }
