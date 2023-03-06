36c8964b1daaa38f5c7f43e8dcda9f17569dbced
katataku <katataku.dev@gmail.com>
2023-03-06 20:50:15 +0900

    block button

--- frontend/src/User/components/Profile/OtherUserProfile.tsx
+++ frontend/src/User/components/Profile/OtherUserProfile.tsx
@@ -1,7 +1,66 @@
-import { type ReactElement, useState, useEffect } from 'react'
+import { type ReactElement, useState, useEffect, useContext } from 'react'
 import { Button } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
+import { GlobalContext } from '../../../App'
+import {
+  deleteChatBlockUserRequest,
+  getChatBlockUserRequest,
+  updateChatBlockUserRequest
+} from '../../../utils/chatBlockUserAxios'
+import { BlockIcon } from '../../../utils/Icon/BlockIcon'
 import { getUserRequest } from '../../../utils/userAxios'
+import { isBlockUser } from '../../utils/userStatusUtils'
+
+function BlockButton(props: { targetUser: User }): ReactElement {
+  const { loginUser } = useContext(GlobalContext)
+  const [blockUserList, setBlockUserList] = useState<blockUserList[]>([])
+  const [isBlocked, setIsBlocked] = useState<boolean>(false)
+
+  const updateIsBlocked = (): void => {
+    getChatBlockUserRequest(loginUser, setBlockUserList)
+  }
+
+  useEffect(() => {
+    setIsBlocked(isBlockUser(props.targetUser, blockUserList))
+  }, [blockUserList])
+
+  useEffect(() => {
+    updateIsBlocked()
+  }, [loginUser])
+
+  return isBlocked ? (
+    <>
+      <BlockIcon isBlocked={isBlocked}></BlockIcon>
+      <Button
+        onClick={() => {
+          const deleteBlockUser: blockUserListPK = {
+            blockUserId: loginUser.id,
+            blockedUserId: props.targetUser.id
+          }
+          deleteChatBlockUserRequest(deleteBlockUser, updateIsBlocked)
+        }}
+      >
+        ブロック解除
+      </Button>
+    </>
+  ) : (
+    <>
+      <Button
+        variant="outline-danger"
+        onClick={() => {
+          const newBlockUser: blockUserList = {
+            blockUserId: loginUser.id,
+            blockedUserId: props.targetUser.id,
+            block_until: new Date(2023, 12, 31, 23, 59, 0)
+          }
+          updateChatBlockUserRequest(newBlockUser, updateIsBlocked)
+        }}
+      >
+        このユーザをブロック
+      </Button>
+    </>
+  )
+}
 
 export function OtherUserProfile(): ReactElement {
   const { id } = useParams()
@@ -30,7 +89,7 @@ export function OtherUserProfile(): ReactElement {
         <Button>このユーザとDMを開始するボタンになる予定</Button>
       </p>
       <p>
-        <Button>このユーザをブロックするボタンになる予定</Button>
+        <BlockButton targetUser={targetUser}></BlockButton>
       </p>
     </>
   )
