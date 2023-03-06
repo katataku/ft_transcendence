188f3f582402926fe53f921971e53a38e6ecf9ae
katataku <katataku.dev@gmail.com>
2023-03-06 22:03:32 +0900

    add tab

--- frontend/src/User/components/Profile/MyPage.tsx
+++ frontend/src/User/components/Profile/MyPage.tsx
@@ -1,14 +1,26 @@
 import { type ReactElement, useContext } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Tab, Tabs } from 'react-bootstrap'
 import { GlobalContext } from '../../../App'
 
-export function MyPage(): ReactElement {
-  const { loginUser } = useContext(GlobalContext)
+function FriendPendingList(): ReactElement {
   return (
     <>
-      <h2>My page</h2>
-      <p>ID : {loginUser.id}</p>
-      <p>NAME: {loginUser.name}</p>
+      <p>ここに招待一覧を表示する</p>
+    </>
+  )
+}
+
+function FriendList(): ReactElement {
+  return (
+    <>
+      <p>ここに友達一覧を表示する</p>
+    </>
+  )
+}
+
+function Settings(): ReactElement {
+  return (
+    <>
       <p>ここにプロフィール画像を表示する</p>
       <p>Gameの成績・Match Historyを表示する</p>
 
@@ -18,6 +30,33 @@ export function MyPage(): ReactElement {
       <p>
         <Button>2FAを有効・無効にするボタンになる予定</Button>
       </p>
+    </>
+  )
+}
+
+export function MyPage(): ReactElement {
+  const { loginUser } = useContext(GlobalContext)
+  return (
+    <>
+      <h2>My page</h2>
+      <p>ID : {loginUser.id}</p>
+      <p>NAME: {loginUser.name}</p>
+
+      <Tabs
+        defaultActiveKey="Settings"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="Settings" title="Settings">
+          <Settings></Settings>
+        </Tab>
+        <Tab eventKey="FriendList" title="FriendList">
+          <FriendList></FriendList>
+        </Tab>
+        <Tab eventKey="FriendPendingList" title="FriendPendingList">
+          <FriendPendingList></FriendPendingList>
+        </Tab>
+      </Tabs>
     </>
   )
 }
