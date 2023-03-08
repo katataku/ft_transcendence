ec50bba09e9c618e0c07ed9fbdde413e13a7948e
katataku <katataku.dev@gmail.com>
2023-03-08 15:18:41 +0900

    friend request button

--- frontend/src/User/components/Profile/OtherUserProfile.tsx
+++ frontend/src/User/components/Profile/OtherUserProfile.tsx
@@ -8,7 +8,14 @@ import {
   updateChatBlockUserRequest
 } from '../../../utils/chatBlockUserAxios'
 import { BlockIcon } from '../../../utils/Icon/BlockIcon'
-import { getUserRequest } from '../../../utils/userAxios'
+import { FriendIcon } from '../../../utils/Icon/FriendIcon'
+import {
+  deleteFriendPendingRequest,
+  getFriendPendingRequest,
+  getFriendsRequest,
+  getUserRequest,
+  updateFriendPendingRequest
+} from '../../../utils/userAxios'
 import { isBlockUser } from '../../utils/userStatusUtils'
 
 function BlockButton(props: { targetUser: User }): ReactElement {
@@ -62,6 +69,81 @@ function BlockButton(props: { targetUser: User }): ReactElement {
   )
 }
 
+export function FriendInvitationButton(props: {
+  targetUser: User
+}): ReactElement {
+  const { loginUser } = useContext(GlobalContext)
+  const [isFriend, setIsFriend] = useState<boolean>(false)
+  const [isPending, setIsPending] = useState<boolean>(false)
+
+  const updateIsFriend = (): void => {
+    getFriendsRequest(loginUser.id, (friends) => {
+      setIsFriend(friends.some((friend) => friend.id === props.targetUser.id))
+    })
+  }
+
+  const updateIsPending = (): void => {
+    getFriendPendingRequest(props.targetUser.id, (friends) => {
+      setIsPending(friends.some((friend) => friend.id === loginUser.id))
+    })
+  }
+
+  useEffect(() => {
+    updateIsFriend()
+    updateIsPending()
+  }, [loginUser, props.targetUser])
+
+  const sendFriendRequest = (): void => {
+    const requestData = {
+      from: loginUser.id,
+      to: props.targetUser.id
+    }
+    updateFriendPendingRequest(requestData, () => {
+      alert('フレンドリクエストを申請しました！')
+      updateIsFriend()
+      updateIsPending()
+    })
+  }
+
+  const sendRejectRequest = (): void => {
+    const requestData = {
+      from: loginUser.id,
+      to: props.targetUser.id
+    }
+    deleteFriendPendingRequest(requestData, () => {
+      alert('フレンドリクエストをキャンセルしました！')
+      updateIsFriend()
+      updateIsPending()
+    })
+  }
+
+  if (isFriend) {
+    return (
+      <>
+        <FriendIcon isFriend={isFriend}></FriendIcon>
+        <Button variant="danger" onClick={() => {}}>
+          フレンドを削除ボタンになる予定
+        </Button>
+      </>
+    )
+  }
+  if (isPending) {
+    return (
+      <>
+        フレンド申請中。
+        <Button variant="danger" onClick={sendRejectRequest}>
+          フレンド申請をキャンセル
+        </Button>
+      </>
+    )
+  }
+  return (
+    <>
+      <Button onClick={sendFriendRequest}>このユーザをフレンドに招待</Button>
+    </>
+  )
+}
+
 export function OtherUserProfile(): ReactElement {
   const { id } = useParams()
 
@@ -83,7 +165,9 @@ export function OtherUserProfile(): ReactElement {
       <p>current statusを表示する(online, offline, in a game,など).</p>
 
       <p>
-        <Button>このユーザをフレンドに招待するボタンになる予定</Button>
+        <FriendInvitationButton
+          targetUser={targetUser}
+        ></FriendInvitationButton>
       </p>
       <p>
         <Button>このユーザとDMを開始するボタンになる予定</Button>
