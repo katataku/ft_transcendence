7da2115176ddd809504274251fd39e38964183d2
katataku <katataku@users.noreply.github.com>
2023-03-12 09:22:37 +0000

    GitHub Actions から差分を Push

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,5 @@
 export const BaseURL: string = process.env
   .REACT_APP_BACKEND_HTTP_BASE_URL as string
 
-  export const chatListKickAlertLocalStorageKey: string = 'ft_trans_chat_list_kick_alert'
+export const chatListKickAlertLocalStorageKey: string =
+  'ft_trans_chat_list_kick_alert'
