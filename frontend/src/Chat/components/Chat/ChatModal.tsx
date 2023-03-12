686886568143a9d8ebdac27c93498b430f80d2df
katataku <katataku.dev@gmail.com>
2023-03-12 18:49:59 +0900

    fix #244

--- frontend/src/Chat/components/Chat/ChatModal.tsx
+++ frontend/src/Chat/components/Chat/ChatModal.tsx
@@ -1,12 +1,42 @@
-import { type ReactElement } from 'react'
+import { useContext, useEffect, useState, type ReactElement } from 'react'
 import { Button, Modal } from 'react-bootstrap'
+import { GlobalContext } from '../../../App'
+import { getChatRoomMembersRequest } from '../../../utils/chatRoomMemberAxios'
 
 export const ChatModal = (props: {
+  room: ChatRoom
   showModal: boolean
   targetUser: User
   handleModalClose: () => void
   handleKickButtonClick: () => void
 }): ReactElement => {
+  const [isAdmin, setIsAdmin] = useState<boolean>(false)
+  const { loginUser } = useContext(GlobalContext)
+
+  useEffect(() => {
+    getChatRoomMembersRequest((members: ChatRoomMember[]) => {
+      members
+        .filter(
+          (member) =>
+            member.userId === loginUser.id &&
+            member.chatRoomId === props.room.id
+        )
+        .forEach((member) => {
+          if (member.isAdministrator) {
+            setIsAdmin(true)
+          }
+        })
+    })
+  }, [props.showModal])
+
+  const kickButton = isAdmin ? (
+    <Button variant="primary" onClick={props.handleKickButtonClick}>
+      Kick
+    </Button>
+  ) : (
+    <></>
+  )
+
   return (
     <>
       <Modal show={props.showModal} onHide={props.handleModalClose}>
@@ -17,9 +47,7 @@ export const ChatModal = (props: {
           <Button variant="secondary" onClick={props.handleModalClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={props.handleKickButtonClick}>
-            Kick
-          </Button>
+          {kickButton}
           <Button variant="primary" onClick={() => {}}>
             ゲーム
           </Button>
