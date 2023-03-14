dbb3de014388bd89bc6ff25b131c979455eebed4
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2023-03-14 21:16:26 +0900

    ...

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './common/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as BodyParser from 'body-parser';
+import * as dotenv from 'dotenv'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  dotenv.config({ path: '.env' });
+  dotenv.config({ path: '.env_secret' });
   app.enableCors(); //CORS
   app.useGlobalPipes(new ValidationPipe());
   app.use(BodyParser.json({ limit: '50mb' }));
--- frontend/src/Auth/auth.ts
+++ frontend/src/Auth/auth.ts
@@ -1,5 +1,4 @@
-const clientId =
-  'u-s4t2ud-ea9deabfb4de552b9d618e65dc6b9f2cf3cc3afe0d38613404f898ac5b43da6b'
+const clientId = process.env.FT_API_CLIENT_ID
 const redirectURI = 'http://localhost:3000/callback'
 const authURL = `https://api.intra.42.fr/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectURI}&response_type=code`
 
