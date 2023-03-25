6f958e831909055c3cd0b0999d2c19444b3f09cf
shima8823 <taiyaki1275@gmail.com>
2023-03-25 11:45:05 +0900

    fix signIn state delete

--- frontend/src/types/common.d.ts
+++ frontend/src/types/common.d.ts
@@ -3,6 +3,4 @@ type Setter<T> = React.Dispatch<React.SetStateAction<T>>
 interface GlobalContext {
   loginUser: User
   setLoginUser: Setter<User>
-  isSignedIn: boolean
-  setIsSignedIn: Setter<boolean>
 }
