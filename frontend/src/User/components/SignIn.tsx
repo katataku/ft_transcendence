a9333cb5d5aa96b78214951ffb2220fc95b1fbc5
shima8823 <taiyaki1275@gmail.com>
2023-03-25 12:57:31 +0900

    fix sign in func

--- frontend/src/User/components/SignIn.tsx
+++ frontend/src/User/components/SignIn.tsx
@@ -3,20 +3,22 @@ import { Form, Button, Image as Img } from 'react-bootstrap'
 import { useState } from 'react'
 import { resizeAndEncode } from '../functions/user.functions'
 import { GlobalContext } from '../../App'
-import {
-  checkUsernameAvailability,
-  signIn,
-  signUp
-} from '../../utils/userAxios'
-import { BaseURL } from '../../constants'
+import { checkUsernameAvailability, signUp } from '../../utils/userAxios'
+import { BaseURL, localStorageKey } from '../../constants'
 import { authenticateWith42 } from '../../Auth/auth'
 import { TwoFactorVerifyModal } from '../../Auth/components/TwoFactorVerifyModal'
-import { getIsTwoFactorEnabled } from '../../utils/authAxios'
+import {
+  getIsTwoFactorEnabled,
+  signIn,
+  validateJwtToken
+} from '../../utils/authAxios'
+import { GameSocketContext } from '../../Game/utils/gameSocketContext'
 
 export const defaultAvatar = `${BaseURL}/user/user_avatar/0`
 
 export function SignIn(): ReactElement {
-  const { setLoginUser, setIsSignedIn } = useContext(GlobalContext)
+  const { setLoginUser } = useContext(GlobalContext)
+  const gameSocket = useContext(GameSocketContext)
   const [signUpMode, setSignUpMode] = useState<boolean>(false)
   const [userName, setUserName] = useState<string>('')
   const [password, setPassword] = useState<string>('')
@@ -38,6 +40,21 @@ export function SignIn(): ReactElement {
 
   function toggleShowPassword(): void {
     setShowPassword(!showPassword)
+  }
+
+  function handleSuccessfulSignIn(res: IlocalStorage): void {
+    localStorage.setItem(localStorageKey, res.access_token)
+    validateJwtToken(
+      (res: jwtPayload) => {
+        const loggedInUser: User = {
+          id: res.userId,
+          name: res.userName
+        }
+        gameSocket.emit('loggedIn', loggedInUser)
+        setLoginUser(loggedInUser)
+      },
+      () => {}
+    )
   }
 
   return (
@@ -117,26 +134,14 @@ export function SignIn(): ReactElement {
                 () => {
                   signUp({ name: userName, password, avatar: image }, (res) => {
                     setLoginUser({ id: res, name: userName })
-                    setIsSignedIn(true)
                   })
                 },
                 () => {
                   alert('Username already exists, so try another username.')
                 }
               )
-            } else {
-              signIn({ name: userName, password }, (res) => {
-                setRegisterUser({ id: res.id, name: res.name })
-                getIsTwoFactorEnabled(res.id, (isTwoFactorEnabled: boolean) => {
-                  if (isTwoFactorEnabled) {
-                    handleTwoFAModalShow()
-                  } else {
-                    setLoginUser({ id: res.id, name: res.name })
-                    setIsSignedIn(true)
-                  }
-                })
-              })
             }
+            signIn({ name: userName, password }, handleSuccessfulSignIn)
           }}
         >
           Submit
@@ -154,8 +159,7 @@ export function SignIn(): ReactElement {
                 data-cy={`login-as-${name}`}
                 variant="warning"
                 onClick={() => {
-                  setLoginUser({ id: index + 1, name })
-                  setIsSignedIn(true)
+                  signIn({ name, password: 'password' }, handleSuccessfulSignIn)
                 }}
               >
                 Login as {name}
@@ -169,7 +173,6 @@ export function SignIn(): ReactElement {
         setShow={setTwoFactorVerifyModalshow}
         handleClose={handleTwoFAModalClose}
         registeringUser={registeringUser}
-        setIsSignedIn={setIsSignedIn}
         setLoginUser={setLoginUser}
       ></TwoFactorVerifyModal>
     </div>
