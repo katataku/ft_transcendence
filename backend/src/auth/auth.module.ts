161a83293d3f790c1e897017f987df6f4b0fa6fc
shima8823 <taiyaki1275@gmail.com>
2023-03-25 11:39:36 +0900

    add jwt auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,10 +3,24 @@ import { UsersModule } from 'src/users';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TwoFactorAuthController } from './twoFactorAuth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      /*
+        useFactoryは動的に生成する。
+        useFactoryがなかった場合、process.env.JWT_SECRET_KEYはundefinedになる。
+      */
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET_KEY,
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
   controllers: [AuthController, TwoFactorAuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
