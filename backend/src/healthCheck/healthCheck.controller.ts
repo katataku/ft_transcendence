fd64554ef1851e0a475ea742d7875e0d6fcb5c12
shima8823 <taiyaki1275@gmail.com>
2023-03-28 19:24:43 +0900

    add / Public decorator

--- backend/src/healthCheck/healthCheck.controller.ts
+++ backend/src/healthCheck/healthCheck.controller.ts
@@ -1,11 +1,13 @@
 import { Get, Controller } from '@nestjs/common';
 import { HealthCheckService } from './healthCheck.service';
 import { HealthDto } from 'src/common/dto/health.dto';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller('health')
 export class HealthCheckController {
   constructor(private service: HealthCheckService) {}
 
+  @Public()
   @Get()
   get(): Promise<HealthDto> {
     return this.service.get();
