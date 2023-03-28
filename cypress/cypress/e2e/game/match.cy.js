590e45feabde9de545a827234f53ef72708b13b0
AuroraSloan <lanajadethompson@gmail.com>
2023-03-29 01:00:06 +0900

    match list view match test

--- cypress/cypress/e2e/game/match.cy.js
+++ cypress/cypress/e2e/game/match.cy.js
@@ -18,29 +18,38 @@ describe('Game - Match Test', () => {
         cy.contains('matching...')
     })
 
+    // DBにguest1とguest2の本当のマッチを実現
     it('can match', () => {
         cy.visit('/')
+        // guest 1 login and join queue
         cy.get('button[data-cy="login-as-guest1"]').click()
         cy.get('a[data-cy="link-to-gamepage"]').click()
         cy.get('button[data-cy="play-button"]').click()
+        // guest 1 logout
         cy.go('back')
         cy.get('button[data-cy="signout-button"]').click()
+        // guest 2 login and join queue
         cy.get('button[data-cy="login-as-guest2"]').click()
         cy.get('a[data-cy="link-to-gamepage"]').click()
         cy.get('button[data-cy="play-button"]').click()
-        cy.contains('Match History') // users have matched
+        // matching successful
+        cy.contains('Match History')
     })
 
     it('can play', () => {
         cy.visit('/')
+        // guest 1 login and click ready (match created above)
         cy.get('button[data-cy="login-as-guest1"]').click()
         cy.get('a[data-cy="link-to-gamepage"]').click()
         cy.get('button[data-cy="ready-button"]').click({ multiple: true })
+        // guest 1 sign out
         cy.go('back')
         cy.get('button[data-cy="signout-button"]').click()
+        // guest 2 login and click ready (match created above)
         cy.get('button[data-cy="login-as-guest2"]').click()
         cy.get('a[data-cy="link-to-gamepage"]').click()
         cy.get('button[data-cy="ready-button"]').click({ multiple: true })
-        cy.contains('3') // countdown
+        // start game (countdown) successful
+        cy.contains('3')
     })
 })
--- cypress/cypress/e2e/game/matchlist.cy.js
+++ cypress/cypress/e2e/game/matchlist.cy.js
@@ -9,4 +9,39 @@ describe('Game - Matchlist Test', () => {
         cy.get('a[data-cy="link-to-matchlist"]').click()
         cy.contains('Ongoing Matches')
     })
+
+    it('can view real match', () => {
+        cy.visit('/')
+        // guest 4 join queue
+        cy.get('button[data-cy="login-as-guest4"]').click()
+        cy.get('a[data-cy="link-to-gamepage"]').click()
+        cy.get('button[data-cy="play-button"]').click()
+        // guest 4 sign out
+        cy.go('back')
+        cy.get('button[data-cy="signout-button"]').click()
+        // guest3 join queue
+        cy.get('button[data-cy="login-as-guest3"]').click()
+        cy.get('a[data-cy="link-to-gamepage"]').click()
+        cy.get('button[data-cy="play-button"]').click()
+        // guest 3 press ready (should be matched with guest 4)
+        cy.get('button[data-cy="ready-button"]').click({ multiple: true })
+        // guest 3 sign out
+        cy.go('back')
+        cy.get('button[data-cy="signout-button"]').click()
+        // log in guest 4 to press ready
+        cy.get('button[data-cy="login-as-guest4"]').click()
+        cy.get('a[data-cy="link-to-gamepage"]').click()
+        cy.get('button[data-cy="ready-button"]').click({ multiple: true })
+        // guest 4 sign out
+        cy.go('back')
+        cy.get('button[data-cy="signout-button"]').click()
+        // guest 1 sign in and view match list
+        cy.get('button[data-cy="login-as-guest1"]').click()
+        cy.get('a[data-cy="link-to-matchlist"]').click()
+        // guest4 vs guest 3 match exists
+        cy.contains('guest4 vs guest3')
+        // view ongoing match
+        cy.get('.list-group-item').contains('guest4 vs guest3').click()
+        cy.contains('Match History')
+    })
 })
