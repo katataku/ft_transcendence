147430f6c0d951073833d1f86bca2aef1bbfe739
katataku <katataku.dev@gmail.com>
2023-03-30 14:07:16 +0900

    fix 299

--- frontend/src/TopPage.tsx
+++ frontend/src/TopPage.tsx
@@ -1,12 +1,25 @@
-import { type ReactElement, useContext } from 'react'
+import { type ReactElement, useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 import { SignIn } from './User/components/SignIn'
 import { GlobalContext } from './App'
 import { initUser, localStorageKey } from './constants'
+import { getAllUsersRequest } from './utils/userAxios'
 
 export function TopPage(): ReactElement {
   const { loginUser, setLoginUser } = useContext(GlobalContext)
+  const [allUserList, setALLUserList] = useState<User[]>([])
+
+  useEffect(() => {
+    // ログインしていないときはユーザ一覧を取得しない。
+    // ログインしていない時はリクエストがエラーとなるため。
+    if (loginUser.id !== 0) {
+      // ユーザ一覧を取得する。
+      getAllUsersRequest((data) => {
+        setALLUserList(data)
+      })
+    }
+  }, [loginUser])
 
   // マッチリスト・プロフィル・チャット->ゲームに行くとmatchIdは必要があります。
   // プロフィル・チャット->ゲームに行くとまだmatchId決めていないので
@@ -33,12 +46,16 @@ export function TopPage(): ReactElement {
               Move to My page (My Profile)
             </Link>
           </p>
-          <p>
-            <Link to={'profile/' + String(1)}>Move to Guest1 Profile</Link>
-          </p>
-          <p>
-            <Link to={'profile/' + String(2)}>Move to Guest2 Profile</Link>
-          </p>
+          {allUserList.map((user) => {
+            if (user.id === loginUser.id) return <></>
+            return (
+              <p key={user.id}>
+                <Link to={'profile/' + String(user.id)}>
+                  Profile: {user.name}(user id: {user.id})
+                </Link>
+              </p>
+            )
+          })}
           <p>
             <Button
               onClick={() => {
