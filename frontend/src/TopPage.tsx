4f165dbf0bccf088750d62952046e137e5b9317a
katataku <katataku.dev@gmail.com>
2023-03-30 15:12:14 +0900

    fix UI

--- frontend/src/TopPage.tsx
+++ frontend/src/TopPage.tsx
@@ -17,20 +17,20 @@ export function TopPage(): ReactElement {
         <div>
           <p>
             <Link data-cy="link-to-chatlist" to="chatlist">
-              Move to ChatList
+              チャットルーム一覧
             </Link>
           </p>
           <p>
-            <Link to="MatchList">Move to MatchList</Link>
+            <Link to="MatchList">実施中のゲームを観戦</Link>
           </p>
           <p>
             <Link to="Game" state={0}>
-              Move to Game
+              Pong ゲームを開始
             </Link>
           </p>
           <p>
             <Link to={'profile/' + String(loginUser.id)}>
-              Move to My page (My Profile)
+              My page (My Profile)
             </Link>
           </p>
           <p>
