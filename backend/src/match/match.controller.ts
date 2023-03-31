66414999438b7b09055d28e3d9370366908e1d86
shima8823 <taiyaki1275@gmail.com>
2023-03-31 11:58:23 +0900

    fix / delete post api

--- backend/src/match/match.controller.ts
+++ backend/src/match/match.controller.ts
@@ -1,15 +1,10 @@
-import { Controller, Post, Get, Body, Param } from '@nestjs/common';
-import { MatchDto, MatchResultDto } from '../common/dto/match.dto';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MatchDto } from '../common/dto/match.dto';
 import { MatchService } from './match.service';
 
 @Controller('match')
 export class MatchController {
   constructor(private service: MatchService) {}
-
-  @Post()
-  createMatch(@Body() match: MatchDto): Promise<MatchDto> {
-    return this.service.createMatch(match);
-  }
 
   @Get('matches')
   getMatches(): Promise<MatchDto[]> {
@@ -19,10 +14,5 @@ export class MatchController {
   @Get(':id')
   getMatchById(@Param('id') id: number): Promise<MatchDto> {
     return this.service.getMatchById(id);
-  }
-
-  @Post('result/:id')
-  postMatchResult(@Body() result: MatchResultDto): Promise<void> {
-    return this.service.postMatchResult(result);
   }
 }
