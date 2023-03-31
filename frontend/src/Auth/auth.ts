7db3ef04b2d1b4cbfe60bcc3c260d92c9fe9d8b7
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2023-03-31 16:02:38 +0900

    環境変数の読み込みを修正

--- frontend/src/Auth/auth.ts
+++ frontend/src/Auth/auth.ts
@@ -1,5 +1,4 @@
-const clientId =
-  'u-s4t2ud-56b91c1f7ae4d450f155af7320a16bce85f13806029385c1a31d4f9bb2279df5' // config/ 以下に記述してコンテナの再起動を行なってもなぜかprocess.envで読み込めないのでベタ書きします。
+const clientId = process.env.REACT_APP_FT_API_CLIENT_ID
 const redirectURI = 'http://localhost:3000/callback'
 const authURL = `https://api.intra.42.fr/oauth/authorize?client_id=${
   clientId as string
