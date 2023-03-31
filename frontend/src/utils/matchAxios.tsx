7b05536d2134528807552e57d148cfeec704a5ee
shima8823 <taiyaki1275@gmail.com>
2023-03-31 11:57:49 +0900

    fix / delete post request

--- frontend/src/utils/matchAxios.tsx
+++ frontend/src/utils/matchAxios.tsx
@@ -1,19 +1,5 @@
 import jwtAxios from './axiosConfig'
 
-export function createMatch(
-  match: MatchDto,
-  callback: (id: number | undefined) => void
-): void {
-  jwtAxios
-    .post<MatchDto>('/match', match)
-    .then((response) => {
-      callback(response.data.id)
-    })
-    .catch((err) => {
-      alert(err)
-    })
-}
-
 export function getMatches(callback: (matches: MatchDto[]) => void): void {
   jwtAxios
     .get<MatchDto[]>('/match/matches')
@@ -34,15 +20,6 @@ export function getMatchById(
     .then((response) => {
       callback(response.data)
     })
-    .catch((err) => {
-      alert(err)
-    })
-}
-
-export function postMatchResult(matchResult: MatchResultDto): void {
-  jwtAxios
-    .post<MatchDto>('/match/result/' + String(matchResult.id), matchResult)
-    .then((_response) => {})
     .catch((err) => {
       alert(err)
     })
