5267ec92b6626744c0b4286b3cd6ba39b73d04f4
kataoka takumi <katataku.dev@gmail.com>
2023-04-02 08:26:45 +0900

    fix (#312)

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -172,11 +172,11 @@ export class UsersService {
     }
   }
 
-  async deletePending(data: FriendRequestDto): Promise<void> {
+  async deletePending(from: number, to: number): Promise<void> {
     const pendingItem = await this.pendingRepository.findOne({
       where: {
-        from: data.from,
-        to: data.to,
+        from: from,
+        to: to,
       },
     });
 
