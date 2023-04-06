d5746d866db0aa29abb5a2eed5c0eeefb5fe613c
shima8823 <taiyaki1275@gmail.com>
2023-04-06 19:32:08 +0900

    add / isNotEmpty validator

--- backend/src/common/dto/auth.dto.ts
+++ backend/src/common/dto/auth.dto.ts
@@ -1,36 +1,51 @@
-import { IsNumber, IsString, IsOptional, IsBoolean } from 'class-validator';
+import {
+  IsNumber,
+  IsString,
+  IsOptional,
+  IsBoolean,
+  IsNotEmpty,
+} from 'class-validator';
 
 export class EnableTwoFactorAuthDto {
+  @IsNotEmpty()
   @IsNumber()
   userId!: number;
 
+  @IsNotEmpty()
   @IsString()
   secret!: string;
 
+  @IsNotEmpty()
   @IsString()
   token!: string;
 }
 
 export class VerifyTwoFactorAuthDto {
+  @IsNotEmpty()
   @IsNumber()
   userId!: number;
 
+  @IsNotEmpty()
   @IsString()
   token!: string;
 }
 
 export class JwtPayloadDto {
+  @IsNotEmpty()
   @IsNumber()
   userId!: number;
 
+  @IsNotEmpty()
   @IsString()
   userName!: string;
 }
 
 export class SigninResDto {
+  @IsNotEmpty()
   @IsNumber()
   userId!: number;
 
+  @IsNotEmpty()
   @IsString()
   userName!: string;
 
@@ -38,6 +53,7 @@ export class SigninResDto {
   @IsString()
   access_token?: string;
 
+  @IsNotEmpty()
   @IsBoolean()
   isTwoFactorEnabled!: boolean;
 }
