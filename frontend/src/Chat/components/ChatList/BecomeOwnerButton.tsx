a89273d076a986f3cc3da6398169a64b3b024ab6
AuroraSloan <lanajadethompson@gmail.com>
2023-04-08 21:05:44 +0900

    add become owner button

---
+++ frontend/src/Chat/components/ChatList/BecomeOwnerButton.tsx
@@ -0,0 +1,10 @@
+import { type ReactElement } from 'react'
+import { Button } from 'react-bootstrap'
+
+export const BecomeOwnerButton = (): ReactElement => {
+  return (
+    <Button variant="info">
+      Become Owner
+    </Button>
+  )
+}
--- frontend/src/Chat/components/ChatList/ChatListDisplay.tsx
+++ frontend/src/Chat/components/ChatList/ChatListDisplay.tsx
@@ -19,6 +19,7 @@ import {
 import { ProtectedIcon } from '../utils/Icon/protectedIcon'
 import { EnterButton } from './EnterButton'
 import { GlobalContext } from '../../../App'
+import {BecomeOwnerButton} from "./BecomeOwnerButton";
 
 // チャットルームに参加するためのボタンを表示する。
 const JoinButton = (props: {
@@ -126,6 +127,7 @@ export const ChatListDisplay = (props: {
             <OwnerIcon isOwner={isOwnerBool}></OwnerIcon>
             <AdminIcon isAdmin={isAdminBool}></AdminIcon>
             <BannedIcon isBanned={isBannedBool}></BannedIcon>
+            <BecomeOwnerButton />
             <EnterButton
               room={room}
               isRoomMember={isRoomMemberBool}
