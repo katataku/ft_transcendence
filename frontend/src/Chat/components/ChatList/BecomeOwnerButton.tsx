7d044f42bc10cb3e9a8f64083230c1a4fca12e94
AuroraSloan <AuroraSloan@users.noreply.github.com>
2023-04-08 12:07:20 +0000

    GitHub Actions から差分を Push

--- frontend/src/Chat/components/ChatList/BecomeOwnerButton.tsx
+++ frontend/src/Chat/components/ChatList/BecomeOwnerButton.tsx
@@ -2,9 +2,5 @@ import { type ReactElement } from 'react'
 import { Button } from 'react-bootstrap'
 
 export const BecomeOwnerButton = (): ReactElement => {
-  return (
-    <Button variant="info">
-      Become Owner
-    </Button>
-  )
+  return <Button variant="info">Become Owner</Button>
 }
